cb5cc6b0d04721325145c4d935a0ed1db544fa35
ThenuwaraHD <hasithdilanthenuwara@gmail.com>
2017-03-02 16:15:48 +0530

    test project

---
+++ src/TestProject/Context/BloggingContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestProject.Models;
+//using TestProject.Models;
+
+namespace TestProject.Context
+{
+    public class BloggingContext : DbContext
+    {
+        public BloggingContext(DbContextOptions<BloggingContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+   
+    }
+}
+
